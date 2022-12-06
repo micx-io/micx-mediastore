76f64dc9881cad22526630df1e25d34efc98d629
Matthias Leuffen <matthes@leuffen.de>
2022-12-06 01:52:35 +0100

    update

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DEV_MODE", (bool)"1");
+define("DEV_MODE", (bool)"0");
 define("CONF_GCLOUD_INDENTY_FILE", "/opt/leuffende-8350153ed12a.json");
 define("CONF_GCLOUD_BUCKET", "micx-mediastore");
 
