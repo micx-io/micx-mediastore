e0d8749454c0070b1db880cefab707eac36b39cd
Matthias Leuffen <matthes@leuffen.de>
2023-03-01 14:14:08 +0100

    set cdn path

--- app.fe/modals/image-details-modal/fragments/overview-tab.ts
+++ app.fe/modals/image-details-modal/fragments/overview-tab.ts
@@ -102,7 +102,7 @@ export class ImageDetailsOverviewFragment extends KaCustomFragment {
                 getCdnGlobUrl: (media) => {
                     let variants = this.scope.media.variant.filter(x => x.variantId !== "preview").map((x) => x.width + "x" + x.height).join(",");
                     let extensions = this.scope.media.variant.filter(x => x.variantId !== "preview")[0].extensions.join(",");
-                    return `${this.scope.index.baseUrl}/v/${this.scope.media.id}/${variants}/${this.scope.media.name}.${extensions}`;
+                    return `cdn:///v/${this.scope.media.id}/${variants}/${this.scope.media.name}.${extensions}`;
                 }
             },
             $on: {
